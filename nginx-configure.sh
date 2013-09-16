2030de57a11bb2a15777b19944541cd05ab4e196
Kevin Jiang <kittymiky@gmail.com>
2013-09-16 09:48:40 +0800

    Update nginx-configure.sh

--- nginx-configure.sh
+++ nginx-configure.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-./configure --prefix=/usr/local/tengine-1.4.2 \
---with-http_stub_status_module \
---with-http_ssl_module \
---with-http_concat_module \
---with-http_realip_modlue
+'./configure' \
+'--prefix=/usr/local/tengine-1.4.2' \
+'--with-http_stub_status_module' \
+'--with-http_ssl_module' \
+'--with-http_concat_module' \
+'--with-http_realip_module' \
+"$@"
